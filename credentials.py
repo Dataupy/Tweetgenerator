3b9d7c46581e9500df8316c6c75bfd64f52361d8
Dataupy <63274926+Dataupy@users.noreply.github.com>
2020-04-06 22:15:34 -0500

    Prearing the API
    Getting ready with the API to track and stored tweets.

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# The consumer key and secret will be generated for you
+API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+API_SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Create an access token under the the "Your access token" section
+ACCESS_TOEKN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
